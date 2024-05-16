821d63e81afc9a05d5bc8e4111e686a3d464a575
Rubén <robben_fly_22@hotmail.com>
2024-05-16 19:55:30 +0200

    Primer commit de la tarea 06 de CD

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,126 @@
+package cuentas;
+/**
+ * @author Ruben
+ * @version 2
+ */
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+    }
+    
+    /** 
+     * @return Retorna el saldo actual de la cuenta bancaria
+     */
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    /**
+     * 
+     * @param cantidad Cantidad de dinero a ingresar
+     * @throws Exception Salta una excepción si se intenta ingresar una cantidad negativa
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     * 
+     * @param cantidad Cantidad de dinero a retirar de la cuenta
+     * @throws Exception Salta una excepción si se intenta retirar una cantidad menor o igual a cero y si la cantidad es mayor que el saldo disponible
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+    /**
+     * 
+     * @return Retorna el nombre del dueño de la cuenta
+     */
+	@SuppressWarnings("unused")
+	private String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * 
+	 * @param nombre Método para modificar el nombre del dueño de la cuenta
+	 */
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * 
+	 * @return Retorna el código de la cuenta bancaria
+	 */
+	@SuppressWarnings("unused")
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	/**
+	 * 
+	 * @param cuenta Método para modificar el código de la cuenta bancaria
+	 */
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	/**
+	 * 
+	 * @return Retorna el saldo actual de la cuenta bancaria
+	 */
+	private double getSaldo() {
+		return saldo;
+	}
+
+	/**
+	 * 
+	 * @param saldo Método para modificar el saldo actual de la cuenta bancaria
+	 */
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	/**
+	 * 
+	 * @return Retrona el tipo de interés de la cuenta bancaria
+	 */
+	@SuppressWarnings("unused")
+	private double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	/**
+	 * 
+	 * @param tipoInterés Método para modificar el tipo de interés de la cuenta bancaria
+	 */
+	@SuppressWarnings("unused")
+	private void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+}
